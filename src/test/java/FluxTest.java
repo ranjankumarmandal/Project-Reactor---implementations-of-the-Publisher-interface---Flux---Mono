576b91671b05bca1c94579df1ffc392e1f370abc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-12-12 18:10:36 +0530

    Project Reactor: completed Flux, and implementation of Publiisher<T> interface for more than one value ie from 0 to N code changes

--- src/test/java/FluxTest.java
+++ src/test/java/FluxTest.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
 
 public class FluxTest {
+
+    // when you have separate datas, use Flux.just() to take data
+    @Test
+    void firstFlux() {
+        Flux.just("Ranjan", "Kumar", "Mandal")
+                .log()
+                .subscribe(System.out::println);
+    }
+
+    // when you have data in a collection format like in ArrayList or something, use Flux.fromIterable() to take data
+    @Test
+    void fluxFromIterable() {
+        Flux.fromIterable(Arrays.asList("Ranjan", "Kumar", "Mandal"))
+                .log()
+                .subscribe(System.out::println);
+    }
+
+    // when you want to take numbers in a given range, use Flux.range(start, count);
+    @Test
+    void fluxFromRange() {
+        Flux.range(10, 5) // 10, 11, 12, 13, 14 (here start is 10 & count is 5)
+                .log()
+                .subscribe(System.out::println);
+    }
 }
